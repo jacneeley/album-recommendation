a3c9477f1bea04cfa87da64670ffa146236a87c4
3ak <jakneeley@outlook.com>
2024-03-22 16:15:06 -0500

    component to grab albums and render them added

--- client/src/components/DisplayAlbums.jsx
+++ client/src/components/DisplayAlbums.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 export default function DisplayAlbums(){
-    //const [albums, setAlbums] = useState([]);
+    const [isLoaded, setLoad] = useState(false);
+    const [albums, setAlbums] = useState({});
 
     //generate random number
     function getRandomInt(max){
         return Math.floor(Math.random() * max);
     }
 
+    const api = process.env.REACT_APP_API_URL;
+
     //fetch album
-    function getRandomAlbum(proxy_url, num){
-        
-        console.log(proxy_url + `${num}`);
+    function getRandomAlbum(num){
+        console.log(api + `${num}`);
         try {
-            fetch(proxy_url + `${num}`, {
+            fetch(api + `${num}`, {
                 headers: {
                     'Accept' : 'application/json',
                     'Content-Type': 'application/json',
@@ -23,20 +25,38 @@ export default function DisplayAlbums(){
             })
             .then(response => response.json())
             .then(albumData => {
+                setLoad(true);
                 console.log(albumData);
+                setAlbums(albumData);
             });
         } catch(err)
         { 
             console.log(err) 
         }
     }
+    
+    // useEffect(() =>{
+    //     console.log(albums);
+    // }, [albums])
 
-    useEffect(() => {
-        getRandomAlbum(process.env.REACT_APP_API_URL, getRandomInt(150));
-    }, [])
 
     return (
         <>
+            <div className="main-content">
+                { !isLoaded ? 
+                    <div className="content">
+                        <p>Need something to listen to?</p>
+                        <button onClick={() => getRandomAlbum(getRandomInt(150))}>shuffle</button>
+                    </div> : 
+                    <div className="content">
+                        <img src={albums.albumPictureUrl}/>
+                        <p>{albums.album}</p>
+                        <p>{albums.artist}</p>
+                        <p>Want something different?</p>
+                        <button onClick={() => getRandomAlbum(getRandomInt(150))}>re-shuffle</button>
+                    </div>
+                } 
+            </div>
         </>
     );
 }
